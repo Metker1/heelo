b5767a0c5e5db7f8b043206e70422bfb3a34336b
Владюха <shapka1.vlad@mail.ru>
2025-05-26 20:10:36 +0400

    Это проверочный код

--- comit.py
+++ comit.py
@@ -1,1 +1,30 @@
-print('Это проверочный комит')
+# Пузырьковый
+import time
+import random
+def bubble(arr):
+    n = len(arr)
+    for i in  range(n):
+        swapped = False
+
+        for j in range(0,n - i -1):
+
+            if arr[j] > arr[j + 1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+                swapped = True
+
+
+        if not swapped:
+            break
+
+
+
+
+arr = []
+for i in range(10000):
+    arr.append(random.randint(1,10000))
+start_time = time.time()
+bubble(arr)
+end_time = time.time()
+elapsed = end_time - start_time
+print(arr)
+print(f'Затраченное время: {elapsed:.4f}')
